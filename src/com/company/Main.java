44e2519285a52a265c1150c562f53f1d972599f5
KateKhodko <kottikate@gmail.com>
2020-09-10 01:26:38 +0300

    Added search methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,44 +6,76 @@ public class Main {
 
     public static void main(String[] args) {
         MyList personList = new MyList();
-        while (true) {
+        do {
             Scanner scan = new Scanner(System.in);
-            System.out.println("1.Ввести абонента\n" +
-                    "2.Выход");
+            System.out.println("1.Добавить абонента\n" +
+                    "2.Вывод списка абонентов\n"+
+                    "3.Поиск абонента по номеру телефона\n" +
+                    "4.Поиск номера телефона по фамилии абонента\n" +
+                    "5.Выход");
             if (scan.hasNextInt()) {
                 int n = scan.nextInt();
                 switch (n) {
                     case 1:
-                        personList.add(inputPerson(personList));
+                        personList.add(inputPersonInfo(personList));
                         break;
                     case 2:
                         sort(personList);
                         print(personList);
+                        break;
+                    case 3:
+                        if (!personList.isEmpty()) {
+                            String number = inputPhoneNumber();
+                            Person person = searchByPhone(number, personList);
+                            if (person != null) {
+                                System.out.println(person.toString());
+                            } else {
+                                System.out.println("Абонент с таким номером не найден");
+                            }
+                        } else {
+                            System.out.println("Список абонентов пуст");
+                        }
+                        break;
+                    case 4:
+                        if (!personList.isEmpty()) {
+                            searchBySurname(personList);
+                        } else {
+                            System.out.println("Список абонентов пуст");
+                        }
+                        break;
+                    case 5:
                         return;
                     default:
                         System.out.println("Пункт меню введен не верно");
                 }
             }
-        }
+        } while (true);
     }
 
-    public static Person inputPerson(MyList personList) {
+    public static Person inputPersonInfo(MyList personList) {
         System.out.println("Введите фамилию");
         String name = inputNSP();
         System.out.println("Введите имя");
         String surname = inputNSP();
         System.out.println("Введите отчество");
         String patronymic = inputNSP();
-        System.out.println("Введите номер телефона");
-        String number = inputNumber();
+        String number = "";
+        while (true) {
+           number = inputPhoneNumber();
+           if (searchByPhone(number, personList) == null) {
+               break;
+           } else {
+               System.out.println("Такой номер телефона уже существует!");
+           }
+        }
         return new Person(surname, name, patronymic, number);
     }
 
-    public static String  inputNSP() {
+    public static String inputNSP() {
         while (true) {
             Scanner scan = new Scanner(System.in);
             String info = scan.nextLine();
-            if (info.matches("(\\s*([А-Яа-я]+)\\s*)+")){
+            if (info.matches("(\\s*([А-Яа-я]+)\\s*)+")) {
                 return info.trim();
             } else {
                 System.out.println("Неккоректный ввод");
@@ -51,19 +83,20 @@ public class Main {
         }
     }
 
-    public static String inputNumber() {
+    public static String inputPhoneNumber() {
+        System.out.println("Введите номер телефона");
         while (true) {
             Scanner scan = new Scanner(System.in);
             String number = scan.nextLine();
-            if (number.matches("(\\s*([0-9]{7})\\s*)+")){
-                return number;
+            if (number.matches("(\\s*([0-9]{7})\\s*)+")) {
+                return number.trim();
             } else {
                 System.out.println("Неккоректный ввод номера телефона");
             }
         }
     }
 
-    public static MyList sort(MyList personList){
+    public static MyList sort(MyList personList) {
         for (int i = 0; i < personList.size(); i++) {
             Person min = personList.get(i);
             int minInd = i;
@@ -73,7 +106,7 @@ public class Main {
                 } else if (min.getSurname().compareToIgnoreCase(personList.get(j).getSurname()) == 0) {
                     if (min.getName().compareToIgnoreCase(personList.get(j).getName()) > 0) {
                         minInd = j;
-                    }  else if (min.getName().compareToIgnoreCase(personList.get(j).getName()) == 0) {
+                    } else if (min.getName().compareToIgnoreCase(personList.get(j).getName()) == 0) {
                         if (min.getPatronymic().compareToIgnoreCase(personList.get(j).getPatronymic()) > 0) {
                             minInd = j;
                         }
@@ -93,10 +126,28 @@ public class Main {
         }
     }
 
-    public static void searchByPhone(MyList personList) {
+    public static Person searchByPhone(String number, MyList personList) {
+        Person person = null;
         for (int i = 0; i < personList.size(); i++) {
-
+            if (personList.get(i).getNumber().equals(number)) {
+                person = personList.get(i);
+            }
         }
+        return person;
     }
 
+    public static void searchBySurname(MyList personList) {
+        System.out.println("Введите фамилию");
+        String surname = inputNSP();
+        boolean flag = false;
+        for (int i = 0; i < personList.size(); i++) {
+            if (personList.get(i).getSurname().equals(surname)) {
+                System.out.println(personList.get(i).toString());
+                flag = true;
+            }
+        }
+        if (!flag) {
+            System.out.println("Номера для абонентов с такой фамилией не найдены");
+        }
+    }
 }
